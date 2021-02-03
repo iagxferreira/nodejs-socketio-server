fd661eb67deb778666778d13a5e5b3df9d998624
Iago Ferreira <iago-ferreira@outlook.com>
2021-02-03 17:02:05 -0300

    add dotenv config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
+require('dotenv/config')
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
