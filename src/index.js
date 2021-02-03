01e087c2ee6f7b4457a33098a005d13fbbac22a3
Iago Ferreira <iago-ferreira@outlook.com>
2021-02-03 17:09:03 -0300

    add chat

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
+const app = require("express")();
+const cors = require("cors");
+app.use(cors());
 
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http);
-require('dotenv/config')
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+require("dotenv/config");
 
-app.get('/', (req, res) => {
-    res.send("Node Server is running. Yay!!")
-})
-
-io.on('connection', socket => {
-    //Get the chatID of the user and join in a room of the same chatID
-    chatID = socket.handshake.query.chatID
-    socket.join(chatID)
-
-    //Leave the room if the user closes the socket
-    socket.on('disconnect', () => {
-        socket.leave(chatID)
-    })
-
-    //Send message to only a particular user
-    socket.on('send_message', message => {
-        receiverChatID = message.receiverChatID
-        senderChatID = message.senderChatID
-        content = message.content
-
-        //Send message to only that particular room
-        socket.in(receiverChatID).emit('receive_message', {
-            'content': content,
-            'senderChatID': senderChatID,
-            'receiverChatID':receiverChatID,
-        })
-    })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/templates/index.html");
 });
 
-http.listen(process.env.PORT)
+io.on("connection", (client) => {
+  client.on("create-room", (data) => {
+    client.join(data.room.id);
+    io.to(data.room.id).emit("new-message", {
+      message: `room ${data.room.id} created successfully`,
+    });
+  });
+
+  client.on("join-room", (data) => {
+    if (data.room.id) {
+      client.join(data.room.id);
+      io.to(data.room.id).emit("new-message", {
+        message: `user ${data.user} connected`,
+      });
+    }
+  });
+
+  client.on("send-message", (data) => {
+    if (data.room.id) {
+      io.to(data.room.id).emit("new-message", data);
+    }
+  });
+
+  client.on("disconnect", () => {
+    io.to(data.room.id).emit("new-message", `user ${client.id} disconnected`);
+  });
+});
+
+server.listen(process.env.PORT || 3000);
